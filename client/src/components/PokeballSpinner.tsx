c1ef47ca29a287995c2353ca8770357eb8a80da9
Kitti Born <born.kitti97@gmail.com>
2024-03-08 17:06:19 +0100

    rename spinner

--- client/src/components/PokeballSpinner.tsx
+++ client/src/components/PokeballSpinner.tsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap";
 
-export default function PokeballSPinner() {
+export default function PokeballSpinner() {
     return (
         <Container className="d-flex justify-content-center mt-3">
             <div className="spinner-grow">
